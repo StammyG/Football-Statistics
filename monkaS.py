5cc14bb51ca9cdfce7a2ea0dfa99d07c4efeb6eb
StammyG <giann.stamoulis@gmail.com>
2024-08-05 12:33:31 +0300

    Add files via upload

--- monkaS.py
+++ monkaS.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 st.title('StammyApp')
 
-db_csv = "Top_Leagues.csv"
+db_csv = "C:/Users/StammyG/Documents/Top_Leagues.csv"
 
 
 @st.cache_data
@@ -29,10 +29,13 @@ competitions = st.sidebar.multiselect(
 )
 competition_filtered_data = data[data['Competition'].isin(competitions)]
 
-
+# Sidebar - Season selection
 seasons = competition_filtered_data['Season'].unique()
 season = st.sidebar.selectbox('Select Season', options=seasons, key='season')
 Season_Filtered_data = competition_filtered_data[competition_filtered_data['Season'] == season]
+
+
+
 
 
 
@@ -44,7 +47,6 @@ opponents = st.sidebar.multiselect(
     'Select Opponents', options=filtered_data['Opponent'].unique(), default=filtered_data['Opponent'].unique()
 )
 filtered_data = filtered_data[filtered_data['Opponent'].isin(opponents)]
-
 
 venues = filtered_data['Venue'].unique()
 venue = st.sidebar.multiselect('Select Venue for Home Team', options=venues, key='venue')
@@ -63,71 +65,67 @@ venues = away_filtered_data['Venue'].unique()
 venue = st.sidebar.multiselect('Select Venue for Away Team', options=venues,)
 venue_away_filtered_data = away_filtered_data [away_filtered_data['Venue'].isin(venue)]
 
-tab1, tab2, tab3 = st.tabs(['Team Stats',"Home Data" ,"Away Data"])
-
-with tab1:
-    st.title('Selected Teams Stats')
-    for_filtered_data = venue_home_filtered_data[venue_home_filtered_data["ForAgainst"] == "For"]
-    against_filtered_data = venue_home_filtered_data[venue_home_filtered_data["ForAgainst"] == "Against"]
-    for_away_filtered_data = venue_away_filtered_data[venue_away_filtered_data["ForAgainst"] == "For"]
-    against_away_filtered_data = venue_away_filtered_data[venue_away_filtered_data["ForAgainst"] == "Against"]
+st.write("Home Data")
+st.dataframe(venue_home_filtered_data)
+st.write("Away Data")
+st.dataframe(venue_away_filtered_data)
 
 
-    def write_mean_stat_to_columns(stat, column, selected_data):
-        stat_mean = selected_data[stat].mean()
-        column.write(f"{stat_mean:.1f}")
+
+st.title('Selected Teams Stats')
+for_filtered_data = venue_home_filtered_data[venue_home_filtered_data["ForAgainst"] == "For"]
+against_filtered_data = venue_home_filtered_data[venue_home_filtered_data["ForAgainst"] == "Against"]
+for_away_filtered_data = venue_away_filtered_data[venue_away_filtered_data["ForAgainst"] == "For"]
+against_away_filtered_data = venue_away_filtered_data[venue_away_filtered_data["ForAgainst"] == "Against"]
 
 
-    def write_stat_to_container(columns_, title, csv_stat, is_against):
-        home_data = venue_home_filtered_data if not is_against else against_filtered_data
-        away_data = venue_away_filtered_data if not is_against else against_away_filtered_data
-        col1_, col2_, col3_ = columns_
-        write_mean_stat_to_columns(csv_stat, col1_, home_data)
-        col2_.write(title)
-        write_mean_stat_to_columns(csv_stat, col3_, away_data)
+def write_mean_stat_to_columns(stat, column, selected_data):
+    stat_mean = selected_data[stat].mean()
+    column.write(f"{stat_mean:.1f}")
 
 
-    st.subheader('Stats for selected teams')
+def write_stat_to_container(columns_, title, csv_stat, is_against):
+    home_data = venue_home_filtered_data if not is_against else against_filtered_data
+    away_data = venue_away_filtered_data if not is_against else against_away_filtered_data
+    col1_, col2_, col3_ = columns_
+    write_mean_stat_to_columns(csv_stat, col1_, home_data)
+    col2_.write(title)
+    write_mean_stat_to_columns(csv_stat, col3_, away_data)
 
-    header_columns = st.columns(3)
-    header_columns[0].write("Home")
-    header_columns[1].write("vs")
-    header_columns[2].write("Away")
 
-    for_stats = st.container()
-    with for_stats:
+st.subheader('Stats for selected teams')
+
+header_columns = st.columns(3)
+header_columns[0].write("Home")
+header_columns[1].write("vs")
+header_columns[2].write("Away")
+
+for_stats = st.container()
+with for_stats:
     columns = st.columns(3)
-        write_stat_to_container(columns, "Shots on Target", "SoT", False)
-        write_stat_to_container(columns, "Shots", "Shots", False)
-        write_stat_to_container(columns, "Tackles", "Tackles", False)
-        write_stat_to_container(columns, "Goal Kicks", "Goal_Kicks", False)
-        write_stat_to_container(columns, "Fouls Commited", "Fouls_Commited", False)
-        write_stat_to_container(columns, "Offsides", "Offsides", False)
-        write_stat_to_container(columns, "Yellow Cards", "Yellow_Cards", False)
-        write_stat_to_container(columns, "xG", "xG", False)
+    write_stat_to_container(columns, "Shots on Target", "SoT", False)
+    write_stat_to_container(columns, "Shots", "Shots", False)
+    write_stat_to_container(columns, "Tackles", "Tackles", False)
+    write_stat_to_container(columns, "Goal Kicks", "Goal_Kicks", False)
+    write_stat_to_container(columns, "Fouls Commited", "Fouls_Commited", False)
+    write_stat_to_container(columns, "Offsides", "Offsides", False)
+    write_stat_to_container(columns, "Yellow Cards", "Yellow_Cards", False)
+    write_stat_to_container(columns, "xG", "xG", False)
+
+st.subheader('Stats against selected teams')
+against_header_columns = st.columns(3)
+against_header_columns[0].write("Home")
+against_header_columns[1].write("vs")
+against_header_columns[2].write("Away")
+against_stats = st.container()
+with against_stats:
+    against_columns = st.columns(3)
+    write_stat_to_container(against_columns, "Shots on Target", "SoT", True)
+    write_stat_to_container(against_columns, "Shots", "Shots", True)
+    write_stat_to_container(against_columns, "Tackles", "Tackles", True)
+    write_stat_to_container(against_columns, "Goal Kicks", "Goal_Kicks", True)
+    write_stat_to_container(against_columns, "Fouls Commited", "Fouls_Commited", True)
+    write_stat_to_container(against_columns, "Offsides", "Offsides", True)
+    write_stat_to_container(against_columns, "Yellow Cards", "Yellow_Cards", True)
+    write_stat_to_container(against_columns, "xG", "xG", True)
     
-    st.subheader('Stats against selected teams')
-    against_header_columns = st.columns(3)
-    against_header_columns[0].write("Home")
-    against_header_columns[1].write("vs")
-    against_header_columns[2].write("Away")
-    against_stats = st.container()
-    with against_stats:
-        against_columns = st.columns(3)
-        write_stat_to_container(against_columns, "Shots on Target", "SoT", True)
-        write_stat_to_container(against_columns, "Shots", "Shots", True)
-        write_stat_to_container(against_columns, "Tackles", "Tackles", True)
-        write_stat_to_container(against_columns, "Goal Kicks", "Goal_Kicks", True)
-        write_stat_to_container(against_columns, "Fouls Commited", "Fouls_Commited", True)
-        write_stat_to_container(against_columns, "Offsides", "Offsides", True)
-        write_stat_to_container(against_columns, "Yellow Cards", "Yellow_Cards", True)
-        write_stat_to_container(against_columns, "xG", "xG", True)
-
-
-with tab2:
-    st.write("Home Data")
-    st.dataframe(venue_home_filtered_data)
-
-with tab3:
-    st.write("Away Data")
-    st.dataframe(venue_away_filtered_data)
