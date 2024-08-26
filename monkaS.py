850d290432b7a49cc4d9a6a2a2b1b1fbe2bc1a9b
Giannis Stamoulis <giann.stamoulis@gmail.com>
2024-08-26 18:38:18 +0300

    Update monkaS.py

--- monkaS.py
+++ monkaS.py
@@ -568,6 +568,9 @@ with TabB:
                 
              </div>
         """, unsafe_allow_html=True)
+        with st.expander(f"Show all matches that {player_name} featured in"):
+         player_matches = filtered_team_roster[filtered_team_roster['player_id'] == player_id]
+         st.write(player_matches)
    
 
         
