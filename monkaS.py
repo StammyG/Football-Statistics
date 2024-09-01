bcf734795d86ee1d1c2080b31c31db84e7aff78e
Giannis Stamoulis <giann.stamoulis@gmail.com>
2024-09-01 23:38:48 +0300

    Update monkaS.py

--- monkaS.py
+++ monkaS.py
@@ -24,9 +24,9 @@ data['Season'] = data['Season'].astype(str)
 YellowCards_Data['Season'] = YellowCards_Data['Season'].astype(str)
 SuperLeague_Data['Season'] = SuperLeague_Data['Season'].astype(str)
 
-player_stats = pd.read_csv('Top5Leagues_player_stats2024.csv')
+player_stats = pd.read_csv('Top5Leagues_player_stats2024_updated.csv')
 player_stats['Team'] = player_stats['Team'].fillna('Random')
-new_order = ['Match','round','minutes','SoT','Shots','Tackles','fouls_commited','fouls_received','Goals','Assists','Team','Competition','Name','Sofascore_Name','player_id',"id"]
+new_order = ['Match','round','minutes','SoT','Shots','Tackles','fouls_commited','fouls_received','Goals','Assists','Team','Competition','Name','Sofascore_Name','player_id']
 player_stats = player_stats[new_order]
 
 #tabs
