262bde85cad288c8b8079d2b4142d9746aeeacbf
Giannis Stamoulis <giann.stamoulis@gmail.com>
2024-09-07 19:50:13 +0300

    Update monkaS.py

--- monkaS.py
+++ monkaS.py
@@ -28,6 +28,7 @@ player_stats = pd.read_csv('Top5Leagues_player_stats2024_updated.csv')
 player_stats['Team'] = player_stats['Team'].fillna('Random')
 new_order = ['Match','round','minutes','SoT','Shots','Tackles','fouls_commited','fouls_received','Goals','Assists','Team','Competition','Name','Sofascore_Name','player_id']
 player_stats = player_stats[new_order]
+data['Date'] = pd.to_datetime(data['Date'])
 
 #tabs
 
