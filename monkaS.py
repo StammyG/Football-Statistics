db83da2804404bba869b2dbc1a2f66f8939cf0c2
Giannis Stamoulis <giann.stamoulis@gmail.com>
2025-01-22 04:01:20 +0200

    Update monkaS.py

--- monkaS.py
+++ monkaS.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 st.title('StammyApp')
 
-db_csv = "Top5_League_Stats_Updated25.csv"
+db_csv = "Top5_League_Stats_Updated26.csv"
 
 @st.cache_data
 def load_data(file_path):
